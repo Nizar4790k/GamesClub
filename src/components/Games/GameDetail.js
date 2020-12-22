460924066301579e365b63757e700c2f2ba14c0e
Nizar <nizar4790k@gmail.com>
2020-12-22 13:42:28 -0400

    get back button is now working

--- src/components/Games/GameDetail.js
+++ src/components/Games/GameDetail.js
@@ -1,18 +1,50 @@
 import React from "react";
 import games from "../../data/game.json";
+import "./GameDetail.css"
+
+import { useHistory } from "react-router-dom";
 
 const GameDetail = ({ match }) => {
   const idx = Number(match.params.id);
   const game = games[idx];
 
+  const history = useHistory();
+  
+  const getBack= function (){
+    history.push('/games');
+  }
+
+
+
+
+
+
+  console.log(game);
+
   return (
     <div className="container">
-      <h2>Game Details</h2>
+      <div className="title">
+      <h1>Titulo:{game.nombre}</h1>
+      </div>
 
-      <p>{game.nombre}</p>
-      <img src={game.img} alt="Game img" />
 
-      <p>Consola: {game.consola}</p>
+      <img src={`${game.img}`} alt="Game img" width="200px" height="400px" />
+
+
+      <h3>Plataforma:{game.consola}</h3>
+      <p>Sobre el juego:{game.info}</p>
+      <h3>Lanzamiento:{game.lanzamiento}</h3>
+     
+    
+
+
+
+    
+      
+      <button onClick={getBack}>Atras</button>
+     
+     
+
     </div>
   );
 };
