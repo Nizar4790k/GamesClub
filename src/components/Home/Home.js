6e8693457501bc1945a1fac3fa2ffda0fa00ca1a
Nizar Sanchez <nizar4790k@gmail.com>
2021-06-23 05:37:10 -0400

    ps4 controller added to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Home.css';
+import img from './ps_controller.png'
+
 
 
 const Home = () =>{
 
     return (
-        <div className="container box">
+        <div className="container">
          <h1>Games Club</h1>
+         <br></br>
+         <img src={img}></img>
+         <br></br>
+         <br></br>
+         <br></br>
          <p>Esta plataforma trata sobre algunos juegos para las consolas Playstation y XBOX</p>
          <button className="btn btn-primary">Ver juegos</button>
          </div>
